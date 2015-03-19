153d9ce86e192f73f2132dc4719735245ce87aed
David Wesst <dw@davidwesst.com>
2015-03-19 13:45:23 -0500

    Added validateFilter function

--- repositories/gamerepository.js
+++ repositories/gamerepository.js
@@ -6,9 +6,45 @@ var azure = require('azure-storage')
 function GameRepository() {
 	var accountName	= nconf.get("STORAGE_NAME")
 		, accountKey = nconf.get("STORAGE_KEY")
-		, tableName= nconf.get("CATALOGUE_TABLE_NAME");
+		, tableName = nconf.get("CATALOGUE_TABLE_NAME");
 
-	azure.createTableService();
+	this.dataService = azure.createTableService();
 };
+
+// Static Functions
+GameRepository.validateFilter = function validateFilter(toValidate) {
+	var validFilterRegex = /(num|new|^[a-zA-Z]{1}$)/g;
+
+	return validFilterRegex.test(toValidate);
+};
+
+// Prototype
+GameRepository.prototype.getGames = function getGames(filter, callback) {
+	var err, query, retVal;
+
+	if(GameRepository.validateFilter(filter.toLowerCase())) {
+		query = new azure.TableQuery()
+					.where("PartitionKey eq ?", filter);
+		this.dataService.queryEntities("games", query, null, function(err, result, response) {
+			// handle error
+			if(err) {
+				callback(err, null);
+			}
+			else {
+				retVal = {};
+				retVal.filter = filter;
+				retVal.results = entities;
+
+				callback(err, retVal);
+			}
+		});
+	}
+	else {
+		err = "Invalid filter: '" + filter + "'";
+		retVal = null;
+
+		callback(err, retVal);
+	}
+}
 
 module.exports = GameRepository;
--- test/gamerepository-spec.js
+++ test/gamerepository-spec.js
@@ -7,27 +7,116 @@ var rewire			= require("rewire");
 var Game			= require("../models/gamemodel");
 var GameRepository	= rewire("../repositories/gamerepository.js");
 
+// mocks
+var azure			= require("azure-storage");
+
 describe("Game Repository", function() {
+	// STUBS
+	var createTableServiceStub
+		, queryEntitiesStub;
 
-	var repository;
+	// SETUP ON ALL TESTS
+	before(function() {
+		queryEntitiesStub		= sinon.stub();
+		var tableServiceMock = {
+			queryEntities: queryEntitiesStub
+		};
+		createTableServiceStub 	= sinon.stub().returns(tableServiceMock);
 
-	describe("is constructed", function() {
+		var azureMock = {
+			createTableService: createTableServiceStub
+		}
+		GameRepository.__set__({
+			"azure.createTableService": createTableServiceStub
+		});
+	});
+
+	afterEach(function() {
+		createTableServiceStub.reset();
+		queryEntitiesStub.reset();
+	});
+
+
+	describe("on initialization", function() {
+		// SPECS
 		it("creates a table service", function() {
-			// setup
-			var createTableServiceStub = sinon.stub().returns();
-			var azureMock =
-				{
-					createTableService: createTableServiceStub
-				};
-			GameRepository.__set__("azure", azureMock);
-
-			repository = new GameRepository();
+			var repository = new GameRepository();
 
 			expect(createTableServiceStub.calledOnce).to.be.true;
 		});
 	});
 
-	describe("#getGames(filter)", function() {
+	describe("#getGames(filter, callback)", function() {
+		it("queries entities from storage", function() {
+			var filter	= "new"
+				, repository = new GameRepository()
+				, cbSpy = sinon.spy();
+			repository.getGames(filter, cbSpy);
 
+			expect(queryEntitiesStub.calledOnce).to.be.true;
+		});
+
+		it("returns error and no results on invalid filter", function(done) {
+			var invalidFilter = ""
+				, repository = new GameRepository()
+				, cbSpy = sinon.spy(function(err, result) {
+							expect(err).to.exist;
+							expect(result).not.to.exist;
+							done();
+							});
+
+			repository.getGames(invalidFilter, cbSpy);
+		});
+	});
+
+	describe("#validateFilter(filter)", function() {
+		it("returns true if filter is 'num', or 'new'", function() {
+			var result;
+
+			result = GameRepository.validateFilter('new');
+			expect(result).to.be.eql(true, "Failed on 'new'");
+
+			result = GameRepository.validateFilter('num');
+			expect(result).to.be.eql(true, "Failed on 'num'");
+
+			// negative tests
+			result = GameRepository.validateFilter('NEW');
+			expect(result).to.be.eql(false, "Failed on 'NEW'");
+
+			result = GameRepository.validateFilter('NUM');
+			expect(result).to.be.eql(false, "Failed on 'NUM'");
+		});
+
+		it("returns true if filter is a single lower case letter", function() {
+			var result;
+
+			result = GameRepository.validateFilter('a');
+			expect(result).to.be.eql(true, "Failed on 'a'");
+
+			result = GameRepository.validateFilter('A');
+			expect(result).to.be.eql(true, "Failed on 'A'");
+
+			// negative testing
+			result =  GameRepository.validateFilter('aa');
+			expect(result).to.be.eql(false, "Failed on 'aa'");
+
+			result =  GameRepository.validateFilter('AA');
+			expect(result).to.be.eql(false, "Failed on 'AA'");
+
+			result =  GameRepository.validateFilter('1');
+			expect(result).to.be.eql(false, "Failed on '1'");
+
+			result =  GameRepository.validateFilter('11');
+			expect(result).to.be.eql(false, "Failed on '11'");
+
+			result =  GameRepository.validateFilter('a1');
+			expect(result).to.be.eql(false, "Failed on 'a1'");
+
+			result =  GameRepository.validateFilter('');
+			expect(result).to.be.eql(false, "Failed on ''");
+
+			result =  GameRepository.validateFilter(1);
+			expect(result).to.be.eql(false, "Failed on 1");
+		});
 	});
 });
