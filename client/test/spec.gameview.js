06268b6dcf192c4fec737ab57c1de3b382a03f1f
David Wesst <dw@davidwesst.com>
2015-04-29 23:54:47 -0500

    Added client test scaffolding

---
+++ client/test/spec.gameview.js
@@ -0,0 +1,13 @@
+/// <reference path="../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../typings/chai/chai.d.ts" />
+/// <reference path="../../typings/sinon/sinon.d.ts" />
+
+describe("GameView", function() {
+	it("should check for a quote in the query string", function() {
+		expect(false).to.be.true;
+	});
+	
+	it("should display the quote with the matching id if it exists on the page");
+	
+	it("should display a 'quote not found' message if a quote does not exist with a matching id");
+});
